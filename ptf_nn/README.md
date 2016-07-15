4968d9b3327df19fc305635e4b440c74ba2f4fbe
Antonin Bas <antonin@barefootnetworks.com>
2016-07-15 16:38:37 -0700

    added ptf nanomsg agent

---
+++ ptf_nn/README.md
@@ -0,0 +1,45 @@
+PTF Nanomsg Agent
+
+---
+
+This agent is able to "bridge" a nanomsg socket with network interfaces. It can
+be useful to enable PTF to send and receive packets on a remote host (i.e. not
+the host running PTF).
+
+---
+
+# Demo
+
+Create the required 2 veth pairs (veth0-veth1 and veth2-veth3) with
+`./veth_setup.sh`.
+
+All of the following 4 steps need to be done in separate terminals. We assume
+that PTF has been installed in a system location (i.e. the `ptf` binary is in
+the `PATH`).
+
+Start the first PTF agent with `sudo python ptf_nn_agent.py --device-socket
+0@tcp://127.0.0.1:10001 -i 0-1@veth0`.
+
+Start the second PTF agent with `sudo python ptf_nn_agent.py --device-socket
+1@tcp://127.0.0.1:10002 -i 1-1@veth3`.
+
+Start the dummy "test switch" with `sudo python ptf_nn_test_bridge.py -ifrom
+veth1 -ito veth2`. This tool will "copy" any packet received on `veth1` to
+`veth2`.
+
+Run the PTF test with `sudo ptf --test-dir ptf_nn_test --device-socket
+0-{0-64}@tcp://127.0.0.1:10001 --device-socket 1-{0-64}@tcp://127.0.0.1:10002
+--platform nn`.
+
+Now let's explain what's happening. We can consider that the first PTF agent
+runs on the PTF host. The second PTF agent runs on the remote host. For each
+host we need to use a separate device id (0 for the PTF host, 1 for the remote
+host). The "switch" is connected to the PTF host through veth0-veth1 and to the
+remote host through veth2-veth3. When running `ptf`, we need to use the `nn`
+platform and provide the nanomsg TCP address for each of the 2 devices.
+
+In our test, we send a packet to port 1 of device 0 and receive the exact same
+packet on port 1 of device 1, as expected.
+
+Of course, the remote host needs to be reachable by the PTF host, or the TCP
+connection is not possible.
---
+++ ptf_nn/ptf_nn_agent.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+
+# Copyright 2013-present Barefoot Networks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Antonin Bas (antonin@barefootnetworks.com)
+#
+#
+
+import sys
+import argparse
+import time
+import struct
+import socket
+import Queue
+try:
+    import nnpy
+except ImportError:
+    print "Cannot find nnpy package, please install"
+    sys.exit(1)
+import threading
+
+# Taken from ptf parser
+class ActionInterface(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        # Parse --interface
+        def check_interface(value):
+            try:
+                dev_and_port, interface = value.split('@', 1)
+                dev_and_port = dev_and_port.split("-")
+                if len(dev_and_port) == 1:
+                    dev, port = 0, int(dev_and_port[0])
+                elif len(dev_and_port) == 2:
+                    dev, port = int(dev_and_port[0]), int(dev_and_port[1])
+                else:
+                    raise ValueError("")
+            except ValueError:
+                parser.error("incorrect interface syntax (got %s, expected 'port@interface' or 'device-port@interface')" % repr(value))
+            return (dev, port, interface)
+
+        assert(type(values) is str)
+        getattr(namespace, self.dest).append(check_interface(values))
+
+class ActionDeviceSocket(argparse.Action):
+    def __init__(self, *args, **kwargs):
+        super(ActionDeviceSocket, self).__init__(*args, **kwargs)
+        self.devices_observed = set()
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # Parse --device-socket
+        def check_device_socket(value):
+            try:
+                dev, addr = value.split('@', 1)
+                dev = int(dev)
+            except ValueError:
+                parser.error("incorrect device-socket syntax (got %s, expected something of the form <dev id>@<socket addr>)" % repr(value))
+            if dev in self.devices_observed:
+                parser.error("cannot specify the same device twice")
+            else:
+                self.devices_observed.add(dev)
+            if addr.startswith("ipc://"):
+                pass
+            elif addr.startswith("tcp://"):
+                pass
+            else:
+                parser.error("nanomsg address must start with 'ipc://' or 'tcp://'")
+            return (dev, addr)
+
+        assert(type(values) is str)
+        getattr(namespace, self.dest).append(check_device_socket(values))
+
+parser = argparse.ArgumentParser(description='PTF Nanomsg agent')
+parser.add_argument(
+    "--device-socket", type=str, dest="device_sockets",
+    metavar="DEVICE-SOCKET", action=ActionDeviceSocket, default=[],
+    help="Specify the nanomsg socket to use to send / receive packets for a given device, as well as the ports to enable on the device.May be given multiple times. Example: 0@<socket addr>")
+parser.add_argument(
+    "--interface", "-i", type=str, dest="interfaces",
+    metavar="INTERFACE", action=ActionInterface, default=[],
+    help="Specify a port number and the dataplane interface to use. May be given multiple times. Example: 0-1@eth2 (use eth2 as port 1 of device 0)")
+args = parser.parse_args()
+
+iface_mgrs = {}
+nano_mgrs = {}
+
+class IfaceMgr(threading.Thread):
+    def __init__(self, dev, port, iface_name):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.dev = dev
+        self.port = port
+        self.iface_name = iface_name
+        self.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,
+                                    socket.htons(0x03))
+        self.socket.bind((iface_name, 0))
+
+    def forward(self, p):
+        # can that conflict with sniff?
+        self.socket.send(p)
+
+    def received(self, p):
+        print "IfaceMgr {}-{} ({}) received a packet".format(
+            self.dev, self.port, self.iface_name)
+        nano_mgr = nano_mgrs[self.dev]
+        nano_mgr.forward(str(p), self.port)
+
+    def run(self):
+        while True:
+            msg = self.socket.recv(4096)
+            self.received(msg)
+
+class NanomsgMgr(threading.Thread):
+    MSG_TYPE_PORT_ADD = 0
+    MSG_TYPE_PORT_REMOVE = 1
+    MSG_TYPE_PORT_SET_STATUS = 2
+    MSG_TYPE_PACKET_IN = 3
+    MSG_TYPE_PACKET_OUT = 4
+
+    def __init__(self, dev, socket_addr):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.dev = dev
+        self.socket_addr = socket_addr
+        self.socket = nnpy.Socket(nnpy.AF_SP, nnpy.PAIR)
+        self.socket.bind(socket_addr)
+
+    def forward(self, p, port):
+        msg = struct.pack("<iii{}s".format(len(p)), self.MSG_TYPE_PACKET_OUT,
+                          port, len(p), p)
+        # because nnpy expects unicode when using str
+        msg = list(msg)
+        self.socket.send(msg)
+
+    def run(self):
+        while True:
+            msg = self.socket.recv()
+            fmt = "<iii"
+            msg_type, port_number, length = struct.unpack_from(fmt, msg)
+            hdr_size = struct.calcsize(fmt)
+            msg = msg[hdr_size:]
+            if (msg_type != self.MSG_TYPE_PACKET_IN):
+                continue
+            assert (len(msg) == length)
+            print "NanomsgMgr {}-{} ({}) received a packet".format(
+                self.dev, port_number, self.socket_addr)
+            iface_mgr = iface_mgrs[(self.dev, port_number)]
+            iface_mgr.forward(msg)
+
+def main():
+    for dev, port, iface in args.interfaces:
+        i = IfaceMgr(dev, port, iface)
+        i.start()
+        iface_mgrs[(dev, port)] = i
+    time.sleep(2)
+    for dev, addr in args.device_sockets:
+        n = NanomsgMgr(dev, addr)
+        n.start()
+        nano_mgrs[dev] = n
+    print "READY"
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        return
+
+if __name__ == '__main__':
+    main()

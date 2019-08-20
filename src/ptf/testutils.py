5f6b8611d7346537f8b35cbd23dd77c2b94b0beb
MELLANOX-zhichenr <zhichenr@mellanox.com>
2019-08-20 09:32:59 -0700

    Add IGMP support for PTF
    Add IGMP support for PTF

--- src/ptf/testutils.py
+++ src/ptf/testutils.py
@@ -1907,6 +1907,80 @@ def simple_qinq_tcp_packet(pktlen=100,
 
     return pkt
 
+def simple_igmp_packet(pktlen=300,
+                       eth_dst='00:01:02:03:04:05',
+                       eth_src='00:06:07:08:09:0a',
+                       dl_vlan_enable=False,
+                       vlan_vid=0,
+                       vlan_pcp=0,
+                       dl_vlan_cfi=0,
+                       ip_src='192.168.0.1',
+                       ip_dst='192.168.0.2',
+                       ip_tos=0,
+                       ip_ecn=None,
+                       ip_dscp=None,
+                       ip_ttl=64,
+                       ip_id=0x0001,
+                       ip_ihl=None,
+                       ip_options=False,
+                       igmp_type=0x12,
+                       igmp_gaddr="224.2.3.4",
+                       igmp_mrtime=64,
+                       inner_frame = None):
+    """
+    Return a simple dataplane IGMP packet
+
+    Supports a few parameters:
+    @param len Length of packet in bytes w/o CRC
+    @param eth_dst Destination MAC
+    @param eth_src Source MAC
+    @param dl_vlan_enable True if the packet is with vlan, False otherwise
+    @param vlan_vid VLAN ID
+    @param vlan_pcp VLAN priority
+    @param ip_src IP source
+    @param ip_dst IP destination
+    @param ip_tos IP ToS
+    @param ip_ecn IP ToS ECN
+    @param ip_dscp IP ToS DSCP
+    @param ip_ttl IP TTL
+    @param ip_id IP ID
+    @param igmp_type IGMP type
+    @param igmp_gaddr IGMP gaddr
+    @param inner_frame The inner Ethernet frame
+
+    Generates a simple IGMP packet. Users shouldn't assume anything about
+    this packet other than that it is a valid ethernet/IP/IGMP frame.
+    """
+    if scapy.IGMP is None:
+        logging.error("An IGMP packet was requested but IGMP is not supported by your Scapy. See README for more information")
+        return None
+
+    if MINSIZE > pktlen:
+        pktlen = MINSIZE
+
+    ip_tos = ip_make_tos(ip_tos, ip_ecn, ip_dscp)
+
+    # Note Dot1Q.id is really CFI
+    if (dl_vlan_enable):
+        pkt = scapy.Ether(dst=eth_dst, src=eth_src)/ \
+            scapy.Dot1Q(prio=vlan_pcp, id=dl_vlan_cfi, vlan=vlan_vid)/ \
+            scapy.IP(src=ip_src, dst=ip_dst, tos=ip_tos, ttl=ip_ttl, id=ip_id, ihl=ip_ihl)
+    else:
+        if not ip_options:
+            pkt = scapy.Ether(dst=eth_dst, src=eth_src)/ \
+                scapy.IP(src=ip_src, dst=ip_dst, tos=ip_tos, ttl=ip_ttl, id=ip_id, ihl=ip_ihl)
+        else:
+            pkt = scapy.Ether(dst=eth_dst, src=eth_src)/ \
+                scapy.IP(src=ip_src, dst=ip_dst, tos=ip_tos, ttl=ip_ttl, id=ip_id, ihl=ip_ihl, options=ip_options)
+
+    pkt = pkt / IGMP(type=igmp_type, gaddr=igmp_gaddr, mrtime=igmp_mrtime)
+
+    if inner_frame:
+        pkt = pkt / inner_frame
+    else:
+        pkt = pkt / simple_tcp_packet(pktlen = pktlen - len(pkt))
+
+    return pkt
 
 """
    DHCP Packet Creation functions

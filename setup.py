4b63876348e173923bbb6c7035ea56057c5bb7eb
Nex Sabre <NexSabre@users.noreply.github.com>
2022-01-14 01:17:02 +0100

    Quality of live improvements for Mask + more tests (#165)
    Add more tests for Mask and introduce MaskException
    
    Bump version to v0.9.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r") as req:
 
 setup(
     name='ptf',
-    version='0.9.2',
+    version='0.9.3',
     description='PTF is a Python based dataplane test framework.',
     long_description=readme,
     long_description_content_type="text/markdown",

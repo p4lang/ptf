c554f83685186be4cfa9387eb5d6d700d2bbd7c0
Wenda Chu <32250288+w1nda@users.noreply.github.com>
2024-04-18 12:45:26 -0700

    Add new initialization arguments for mask class to support set mask with less code (#206)
    * init mask with dont care all
    
    * Reorder functions to get better diff display
    
    * Reorder functions to get better diff display
    
    * Reorder functions to get better diff display

--- src/ptf/mask.py
+++ src/ptf/mask.py
@@ -12,12 +12,27 @@ class MaskException(Exception):
 
 
 class Mask:
-    def __init__(self, exp_pkt, ignore_extra_bytes=False):
+    def __init__(self, exp_pkt, ignore_extra_bytes=False, dont_care_all=False):
         self.exp_pkt = exp_pkt
         self.size = len(exp_pkt)
         self.valid = True
-        self.mask = [0xFF] * self.size
+        self.mask = [0] * self.size if dont_care_all else [0xFF] * self.size
         self.ignore_extra_bytes = ignore_extra_bytes
+
+    def set_care(self, offset, bitwidth):
+        for idx in range(offset, offset + bitwidth):
+            offsetB = idx // 8
+            offsetb = idx % 8
+            self.mask[offsetB] = self.mask[offsetB] | (1 << (7 - offsetb))
+
+    def set_care_all(self):
+        self.mask = [0xFF] * self.size
+
+    def set_care_packet(self, hdr_type, field_name):
+        offset, bitwidth = self._calculate_fields_offset_and_bitwidth(
+            hdr_type, field_name
+        )
+        self.set_care(offset, bitwidth)
 
     def set_do_not_care(self, offset, bitwidth):
         # a very naive but simple method
@@ -27,7 +42,45 @@ class Mask:
             offsetb = idx % 8
             self.mask[offsetB] = self.mask[offsetB] & (~(1 << (7 - offsetb)))
 
+    def set_do_not_care_all(self):
+        self.mask = [0] * self.size
+
     def set_do_not_care_packet(self, hdr_type, field_name):
+        offset, bitwidth = self._calculate_fields_offset_and_bitwidth(
+            hdr_type, field_name
+        )
+        self.set_do_not_care(offset, bitwidth)
+
+    def set_do_not_care_scapy(self, hdr_type, field_name):
+        warnings.warn(
+            '"set_do_not_care_scapy" is going to be deprecated, please '
+            'switch to the new one: "set_do_not_care_packet"',
+            DeprecationWarning,
+        )
+        self.set_do_not_care_packet(hdr_type, field_name)
+
+    def set_ignore_extra_bytes(self):
+        self.ignore_extra_bytes = True
+
+    def is_valid(self):
+        return self.valid
+
+    def pkt_match(self, pkt):
+        # just to be on the safe side
+        pkt = bytearray(bytes(pkt))
+        # we fail if we don't match on sizes, or if ignore_extra_bytes is set,
+        # fail if we have not received at least size bytes
+        if (not self.ignore_extra_bytes and len(pkt) != self.size) or len(
+            pkt
+        ) < self.size:
+            return False
+        exp_pkt = bytearray(bytes(self.exp_pkt))
+        for i in range(self.size):
+            if (exp_pkt[i] & self.mask[i]) != (pkt[i] & self.mask[i]):
+                return False
+        return True
+
+    def _calculate_fields_offset_and_bitwidth(self, hdr_type, field_name):
         if hdr_type not in self.exp_pkt:
             self.valid = False
             raise MaskException("Unknown header type")
@@ -64,36 +117,7 @@ class Mask:
                 break
             else:
                 offset += bits
-        self.set_do_not_care(hdr_offset * 8 + offset, bitwidth)
-
-    def set_do_not_care_scapy(self, hdr_type, field_name):
-        warnings.warn(
-            '"set_do_not_care_scapy" is going to be deprecated, please '
-            'switch to the new one: "set_do_not_care_packet"',
-            DeprecationWarning,
-        )
-        self.set_do_not_care_packet(hdr_type, field_name)
-
-    def set_ignore_extra_bytes(self):
-        self.ignore_extra_bytes = True
-
-    def is_valid(self):
-        return self.valid
-
-    def pkt_match(self, pkt):
-        # just to be on the safe side
-        pkt = bytearray(bytes(pkt))
-        # we fail if we don't match on sizes, or if ignore_extra_bytes is set,
-        # fail if we have not received at least size bytes
-        if (not self.ignore_extra_bytes and len(pkt) != self.size) or len(
-            pkt
-        ) < self.size:
-            return False
-        exp_pkt = bytearray(bytes(self.exp_pkt))
-        for i in range(self.size):
-            if (exp_pkt[i] & self.mask[i]) != (pkt[i] & self.mask[i]):
-                return False
-        return True
+        return hdr_offset * 8 + offset, bitwidth
 
     def __str__(self):
         old_stdout = sys.stdout

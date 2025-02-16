78a6d6b240051de4fab98b1a80f719eca78f0352
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-02-16 15:31:16 +0000

    More setting of PYTHONPATH in sudo commands where needed
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -2,26 +2,28 @@
 
 set -x
 
-sudo python ptf_nn/ptf_nn_agent.py \
+PPATH="/home/runner/.local/lib/python3.12/site-packages"
+
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python ptf_nn/ptf_nn_agent.py \
     --device-socket 0@tcp://127.0.0.1:10001 -i 0-1@veth0 \
     &
 
 sleep 5
 
-sudo python ptf_nn/ptf_nn_agent.py \
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python ptf_nn/ptf_nn_agent.py \
     --device-socket 1@tcp://127.0.0.1:10002 -i 1-1@veth3 \
     &
 
 sleep 5
 
-sudo python ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
     &
 
 sleep 5
 
 env
 
-sudo PATH=${PATH} PYTHONPATH="/home/runner/.local/lib/python3.12/site-packages" `which ptf` --test-dir ptf_nn/ptf_nn_test \
+sudo PATH=${PATH} PYTHONPATH=${PPATH} `which ptf` --test-dir ptf_nn/ptf_nn_test \
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \
     --platform nn

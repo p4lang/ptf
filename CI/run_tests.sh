affca3397aed56f9df2eddd834d7766e11983aa0
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-02-16 15:36:46 +0000

    More tweaks to try to get all tests to pass
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -2,21 +2,21 @@
 
 set -x
 
-PPATH="/home/runner/.local/lib/python3.12/site-packages"
+PPATH="$HOME/.local/lib/python3.12/site-packages"
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python ptf_nn/ptf_nn_agent.py \
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 ptf_nn/ptf_nn_agent.py \
     --device-socket 0@tcp://127.0.0.1:10001 -i 0-1@veth0 \
     &
 
 sleep 5
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python ptf_nn/ptf_nn_agent.py \
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 ptf_nn/ptf_nn_agent.py \
     --device-socket 1@tcp://127.0.0.1:10002 -i 1-1@veth3 \
     &
 
 sleep 5
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
     &
 
 sleep 5
@@ -27,3 +27,5 @@ sudo PATH=${PATH} PYTHONPATH=${PPATH} `which ptf` --test-dir ptf_nn/ptf_nn_test 
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \
     --platform nn
+
+sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 `which nose2` utests.tests.test

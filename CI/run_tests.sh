c8ab50f18e1ea7dab74d618e2eb0fd8cc6c08ff4
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-02-18 16:02:57 +0000

    Fix typo
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -19,8 +19,6 @@ sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth
 
 sleep 5
 
-env
-
 sudo -E PATH=${PATH} `which ptf` --test-dir ptf_nn/ptf_nn_test \
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \

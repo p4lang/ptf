c138ccae8e2aa27368fbfd7071e86bca0413c5ba
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-02-18 15:55:12 +0000

    Reverting previous change, which led to failing CI tests
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -2,28 +2,28 @@
 
 set -x
 
-sudo --preserve-env=PATH python3 ptf_nn/ptf_nn_agent.py \
+sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_agent.py \
     --device-socket 0@tcp://127.0.0.1:10001 -i 0-1@veth0 \
     &
 
 sleep 5
 
-sudo --preserve-env=PATH python3 ptf_nn/ptf_nn_agent.py \
+sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_agent.py \
     --device-socket 1@tcp://127.0.0.1:10002 -i 1-1@veth3 \
     &
 
 sleep 5
 
-sudo --preserve-env=PATH python3 ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
+sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
     &
 
 sleep 5
 
 env
 
-sudo --preserve-env=PATH `which ptf` --test-dir ptf_nn/ptf_nn_test \
+sudo -E PATH=${PATH} `which ptf` --test-dir ptf_nn/ptf_nn_test \
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \
     --platform nn
 
-sudo --preserve-env=PATH python3 `which nose2` utests.tests.test
+sudo -E PATH=${PATH} python3 `which nose2` utests.tests.test

75bfc783add9c8aec965e8850775deb3fa804408
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-02-18 14:41:50 +0000

    Try sudo -E for running Python code as root
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -2,31 +2,28 @@
 
 set -x
 
-MAJOR_MINOR_VERSION=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
-PPATH="$HOME/.local/lib/python${MAJOR_MINOR_VERSION}/site-packages"
-
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 ptf_nn/ptf_nn_agent.py \
+sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_agent.py \
     --device-socket 0@tcp://127.0.0.1:10001 -i 0-1@veth0 \
     &
 
 sleep 5
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 ptf_nn/ptf_nn_agent.py \
+sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_agent.py \
     --device-socket 1@tcp://127.0.0.1:10002 -i 1-1@veth3 \
     &
 
 sleep 5
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
+sudo -E PATH=${PATH} python3 ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
     &
 
 sleep 5
 
 env
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} `which ptf` --test-dir ptf_nn/ptf_nn_test \
+sudo -E PATH=${PATH} `which ptf` --test-dir ptf_nn/ptf_nn_test \
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \
     --platform nn
 
-sudo PATH=${PATH} PYTHONPATH=${PPATH} python3 `which nose2` utests.tests.test
+sudo -E PATH=${PATH} python3 `which nose2` utests.tests.test

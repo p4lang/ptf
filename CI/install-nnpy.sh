616c52bba85068b591e6e654ab89010225b02797
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2025-03-03 00:34:23 -0500

    Try a simpler way to install nnpy that seems to work for BMv2
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/install-nnpy.sh
+++ CI/install-nnpy.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 set -e
-git clone https://github.com/nanomsg/nnpy.git
-cd nnpy
-python3 -m pip install cffi
-python3 -m pip install --upgrade cffi
-python3 -m pip install .
-cd ..
+pip3 install nnpy cffi

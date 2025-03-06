ce1f04efab4345c9519ccf56db54cd193114b72b
Andy Fingerhut <andy.fingerhut@gmail.com>
2025-03-06 10:15:06 -0500

    Revert install-nnpy.sh back to original
    Signed-off-by: Andy Fingerhut <andy.fingerhut@gmail.com>

--- CI/install-nnpy.sh
+++ CI/install-nnpy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
+git clone https://github.com/nanomsg/nnpy.git
+cd nnpy
 python3 -m pip install cffi
-python3 -m pip install nnpy
+python3 -m pip install --upgrade cffi
+python3 -m pip install .
+cd ..

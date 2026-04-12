d76418f86b5d79df0cb9cd5a7f98c3a472bfa76c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-04-12 06:06:10 +0000

    Add copyright and Apache 2.0 license to many source files In this commit, all copyright notices added are for the name and year from the first commit in the ptf repository that added the file to the ptf repository.
    Files derived from the oftest Github repository will be modified in a
    separate PR.
    
    bf_pktpy files copied from the p4lang/open-p4studio repository will be
    modified in a separate commit in this PR.
    
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- example/mytests/switch.py
+++ example/mytests/switch.py
@@ -1,4 +1,4 @@
-# Copyright 2013-present Barefoot Networks, Inc.
+# Copyright 2013 Barefoot Networks, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
 
 """
 Thrift SAI interface basic tests

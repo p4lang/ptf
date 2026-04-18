a40adf58c83885f98cc7016e58c6638ff6af3341
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-04-18 18:30:36 +0000

    Try a different way to run uv-installed .venv commands as root
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -5,26 +5,26 @@
 
 set -x
 
-sudo -E PATH=${PATH} uv run ptf_nn/ptf_nn_agent.py \
+sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" ptf_nn/ptf_nn_agent.py \
     --device-socket 0@tcp://127.0.0.1:10001 -i 0-1@veth0 \
     &
 
 sleep 5
 
-sudo -E PATH=${PATH} uv run ptf_nn/ptf_nn_agent.py \
+sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" ptf_nn/ptf_nn_agent.py \
     --device-socket 1@tcp://127.0.0.1:10002 -i 1-1@veth3 \
     &
 
 sleep 5
 
-sudo -E PATH=${PATH} uv run ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
+sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
     &
 
 sleep 5
 
-sudo -E PATH=${PATH} uv run `which ptf` --test-dir ptf_nn/ptf_nn_test \
+sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/ptf" --test-dir ptf_nn/ptf_nn_test \
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \
     --platform nn
 
-sudo -E PATH=${PATH} uv run `which nose2` utests.tests.test
+sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" `which nose2` utests.tests.test
--- CI/uv-setup-env.bash
+++ CI/uv-setup-env.bash
@@ -1,1 +1,2 @@
-export PATH="${PATH}:${HOME}/.local/bin"
+UV_VENV_BIN_DIR="${HOME}/.local/bin"
+export PATH="${PATH}:${UV_VENV_BIN_DIR}"

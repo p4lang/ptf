6d90667238d3807918885f27072aa79ac0968072
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-04-18 21:50:30 +0000

    Experiment to see if this will work.
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- CI/run_tests.sh
+++ CI/run_tests.sh
@@ -7,26 +7,26 @@ set -x
 
 . CI/uv-setup-env.bash
 
-sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" ptf_nn/ptf_nn_agent.py \
+sudo "${UV_VENV_BIN_DIR}/uv" run ptf_nn/ptf_nn_agent.py \
     --device-socket 0@tcp://127.0.0.1:10001 -i 0-1@veth0 \
     &
 
 sleep 5
 
-sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" ptf_nn/ptf_nn_agent.py \
+sudo "${UV_VENV_BIN_DIR}/uv" run ptf_nn/ptf_nn_agent.py \
     --device-socket 1@tcp://127.0.0.1:10002 -i 1-1@veth3 \
     &
 
 sleep 5
 
-sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
+sudo "${UV_VENV_BIN_DIR}/uv" run ptf_nn/ptf_nn_test_bridge.py -ifrom veth1 -ito veth2 \
     &
 
 sleep 5
 
-sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/ptf" --test-dir ptf_nn/ptf_nn_test \
+sudo "${UV_VENV_BIN_DIR}/uv" run ptf --test-dir ptf_nn/ptf_nn_test \
     --device-socket 0-{0-64}@tcp://127.0.0.1:10001 \
     --device-socket 1-{0-64}@tcp://127.0.0.1:10002 \
     --platform nn
 
-sudo -E PATH=${PATH} "${UV_VENV_BIN_DIR}/python3" `which nose2` utests.tests.test
+sudo "${UV_VENV_BIN_DIR}/uv" run nose2 utests.tests.test
